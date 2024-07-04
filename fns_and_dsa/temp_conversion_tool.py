c78a19ff58e3fe9a893127e7de683e4b62ca2293
Tobi <markinstobi@gmail.com>
2024-07-04 09:54:07 +0100

    second ammendment

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,12 +3,12 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     #global FAHRENHEIT_TO_CELSIUS_FACTOR
-    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32) 
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR 
     return celsius
 
 def convert_to_fahrenheit(celsius):
     #global CELSIUS_TO_FAHRENHEIT_FACTOR
-    fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
+    fahrenheit = 32 + (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR)
     return fahrenheit
 
 temp = int(input('Enter the temperature to convert: '))
@@ -17,10 +17,10 @@ temp_unit = input('Is this temperature in Celsius or Fahrenheit? (C/F): ').strip
 match temp_unit:
     case "F":
         celsius = convert_to_celsius(temp)
-        print(f"{temp}{chr(176)}F is {celsius}")
+        print(f"{temp}{chr(176)}F is {celsius}{chr(176)}C")
     case "C":
         farhenheit = convert_to_fahrenheit(temp)
-        print(f"{temp}\N{DEGREE SIGN}C is {farhenheit}")
+        print(f"{temp}\N{DEGREE SIGN}C is {farhenheit}\N{DEGREE SIGN}F")
     case _:
         print("Enter the correct letter F or C")
         #quit()
