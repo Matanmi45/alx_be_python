56bd27587e1b8610b7df916d691c98cad48ce43a
Tobi <markinstobi@gmail.com>
2024-07-10 09:37:09 +0100

    week 6

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -22,26 +22,25 @@ class Library():
     
     
     def __init__(self):
-        self._book = []
+        self._books = []
         self.copy_book = []
     
     def add_book(self, book):
-        self._book.append(book)
-        self.copy_book = copy.deepcopy(self._book)
+        self._books.append(book)
+        self.copy_book = copy.deepcopy(self._books)
     
     def check_out_book(self, title):
-        for book in self._book:
+        for book in self._books:
             if title in str(book):
-                self._book.remove(book)
+                self._books.remove(book)
                 #self._book[2] = True
         
     def return_book(self, title):
         for copy_book in self.copy_book:
-            for book in self._book:
+            for book in self._books:
                 if copy_book != book and title in str(copy_book):
-                    self._book.append(copy_book)
+                    self._books.append(copy_book)
               
     def list_available_books(self):
-        for book in self._book:
+        for book in self._books:
             print(book)
-        
